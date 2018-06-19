59f5c5a74afa280dd7a90aa4c6a96102aaecbd48
Juan <astray@Juans-MBP.attlocal.net>
2018-06-18 21:16:18 -0700

    Readme Update

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-print "Hello World!"
-
-import os
-#os.system("ps -A")
-
 import commands
 status, output = commands.getstatusoutput("ps -A")
+
+#print output
+
+#print output[0]
+
+
+test = output.split('\n')
+
+print test
+
+print "*****************************************"
+
+print test[0]
+print test[1]
+print test[2]
+#print test[3]
+#print test [4]
